9e9da68f61d6c80a4c50810a8cbd0c03b13105dd
FannyHunin <fanny.hunin.an123@gmail.com>
2020-10-07 10:17:46 +0200

    reception fichiers jordano, style terminé

--- public/js/modeSombre.js
+++ public/js/modeSombre.js
@@ -2,11 +2,11 @@ let divModeSombre = document.getElementsByClassName("li-sectionHeaderLast")[0];
 let myBody = document.querySelector("body");
 let header = document.getElementById("navBar");
 let divSct3 = document.getElementsByClassName("divContainer-section3")[0]
-console.log(header)
-
+let section5 = document.getElementById("section5")
 
 divModeSombre.addEventListener("click", () => {
     myBody.classList.toggle("classModeSombre");
     header.classList.toggle("classModeSombre");
     divSct3.classList.toggle("classModeSombre");
+    section5.classList.toggle("classModeSombre");
 })
